f265aa03a0a56f42b0307da8a5287683ba2f164c
baffioso <baffyx@gmail.com>
2022-05-20 21:04:03 +0200

    gh pages

---
+++ src/assets/treasures.ts
@@ -0,0 +1,46 @@
+import { FeatureCollection } from 'geojson'
+
+export const treasures: FeatureCollection = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "bogstav": "a"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.577564716339111,
+          55.667940406235374
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "bogstav": "b"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.576497197151184,
+          55.66745935379183
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "bogstav": "e"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.57591515779495,
+          55.66709931696172
+        ]
+      }
+    }
+  ]
+}
